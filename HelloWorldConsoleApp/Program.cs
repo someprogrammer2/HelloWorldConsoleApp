0a44894c50f43b61c734d226678386f4a6cbf8ab
someprogrammer2 <someprogrammer2@mail.ru>
2023-12-09 09:37:59 +0500

    Добавлен метод для приветствия по имени - SayHello

--- HelloWorldConsoleApp/Program.cs
+++ HelloWorldConsoleApp/Program.cs
@@ -5,6 +5,13 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            SayHello("Oleg");
         }
+
+        static void SayHello(string name)
+        {
+            Console.WriteLine($"Hello, {name}");
+        }
+
     }
 }
